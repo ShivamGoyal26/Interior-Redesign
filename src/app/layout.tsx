2ee6af3f483d911e5224e1e8d6072705b5204001
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-11-12 22:03:34 +0530

    Sign & Signup page added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
@@ -36,8 +37,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${outfit.className}`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${outfit.className}`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
