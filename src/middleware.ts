d914b8eeb1aef60434abaee61b0f18e77416a27b
shivamgoyal26 <officialshivamgoyal@gmail.com>
2024-11-26 23:02:42 +0530

    middleware redirect added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,23 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isRootRoute = createRouteMatcher(["/"]); // Added matcher for root route
 
 export default clerkMiddleware(async (auth, request) => {
+  if (isRootRoute(request)) {
+    // Construct the absolute URL for redirect
+    const url = request.nextUrl.clone();
+    url.pathname = "/dashboard"; // Set the path to /dashboard
+    return NextResponse.redirect(url); // Redirect to the absolute URL
+  }
   if (!isPublicRoute(request)) {
+    // Protect all non-public routes
     await auth.protect();
   }
+
+  // Returning response for the next middleware or request handler
+  return NextResponse.next();
 });
 
 export const config = {
